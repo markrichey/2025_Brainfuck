0e7b39efe2a0eff1e9bcce0cb4a9b3698cb9cf3d
Mark Richey <mkrichey@gmail.com>
2025-02-24 08:23:05 +0000

    Test 001 Completed

--- brainfuck.py
+++ brainfuck.py
@@ -2,11 +2,31 @@ with open('test001.bf', 'r') as file:
     
     valueArray = [0]
     valuePointer = 0
+    loopingArray = []
     
     for line in file:
+        
         stripLine = line.strip()
         stripLineArray = list(stripLine)
         
         for cmd in stripLineArray:
             if cmd in (">","<","+","-",".",",","[","]"):
-                print(cmd)
+                
+                if cmd == ">":
+                    valuePointer += 1
+                    if len(valueArray) < (valuePointer + 1):
+                        valueArray.append(0)
+                elif cmd == "<":
+                    valuePointer -= 1
+                    if len(valueArray) < (valuePointer + 1):
+                        valueArray.append(0)
+
+                elif cmd == "+":
+                    valueArray[valuePointer] += 1
+                elif cmd == "-":
+                    valueArray[valuePointer] -= 1
+                elif cmd == ".":
+                    print(valueArray[valuePointer])
+
+    print(valueArray)
+                
