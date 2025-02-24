cab0c4d50849f311ed6d4cbfc98a3adc50064264
Mark Richey <mkrichey@gmail.com>
2025-02-24 08:47:49 +0000

    Passes Test 002

--- brainfuck_test002.py
+++ brainfuck_test002.py
@@ -1,43 +1,53 @@
-with open('test001.bf', 'r') as file:
+with open('test002.bf', 'r') as file:
     
     valueArray = [0]
     valuePointer = 0
+    loopPositionArray = []
     
+    singleLineProg = ""
     for line in file:
-        
         stripLine = line.strip()
-        stripLineArray = list(stripLine)
+        singleLineProg = singleLineProg + stripLine
 
-        maxC = len(stripLineArray)
-        currentC = -1
+    stripLineArray = list(singleLineProg)
+    currentC = -1
+    maxC = len(stripLineArray)
+    print("Max C:",maxC)
+
+    while currentC < maxC:
+        currentC += 1
+
+        try:
+
+            if stripLineArray[currentC] in (">","<","+","-",".",",","[","]"):
+                
+                if stripLineArray[currentC] == ">":
+                    valuePointer += 1
+                    if len(valueArray) < (valuePointer + 1):
+                        valueArray.append(0)
+                elif stripLineArray[currentC] == "<":
+                    valuePointer -= 1
+                    if len(valueArray) < (valuePointer + 1):
+                        valueArray.append(0)
+                elif stripLineArray[currentC] == "+":
+                    valueArray[valuePointer] += 1
+                elif stripLineArray[currentC] == "-":
+                    valueArray[valuePointer] -= 1
+                elif stripLineArray[currentC] == ".":
+                    print(valueArray[valuePointer])
+                elif stripLineArray[currentC] == "[":
+                    loopPositionArray.append(currentC)
+                    print(loopPositionArray)
+                elif stripLineArray[currentC] == "]":
+                    if valueArray[valuePointer] == 0:
+                        loopPositionArray.pop()
+                        print(loopPositionArray)
+                    else:
+                        print("Set currentC to ",loopPositionArray[len(loopPositionArray) - 1])
+                        currentC = loopPositionArray[len(loopPositionArray) - 1]
+
+        except:
+            break
         
-        while currentC < maxC:
-
-            currentC += 1
-
-            try:
-
-                if stripLineArray[currentC] in (">","<","+","-",".",",","[","]"):
-                    
-                    if stripLineArray[currentC] == ">":
-                        valuePointer += 1
-                        if len(valueArray) < (valuePointer + 1):
-                            valueArray.append(0)
-                    elif stripLineArray[currentC] == "<":
-                        valuePointer -= 1
-                        if len(valueArray) < (valuePointer + 1):
-                            valueArray.append(0)
-
-                    elif stripLineArray[currentC] == "+":
-                        valueArray[valuePointer] += 1
-                    elif stripLineArray[currentC] == "-":
-                        valueArray[valuePointer] -= 1
-                    elif stripLineArray[currentC] == ".":
-                        print(valueArray[valuePointer])
-
-            except:
-                break
-
-
     print(valueArray)
                 
